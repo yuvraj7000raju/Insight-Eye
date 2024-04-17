4a63e3d4e5170687a9aab04931bdf3e2b61241e3
yuvraj yadav <yuvraj7000raju@gmail.com>
2024-04-18 00:31:28 +0530

    backend initial setup is complete , database is connected

---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv"
+dotenv.config({
+    path : '../.env'
+})
+import app from "./app.js"
+import mongoose from "mongoose"
+
+
+(
+  async()=>{
+    try{
+    await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DATABASE}`)
+    app.listen(process.env.PORT , ()=>{
+        console.log(`app is listning on port : ${process.env.port}`)
+    })
+    }catch(error){
+        console.log("error here --------")
+        console.log(error)
+        throw error
+    }
+  }
+)()
+
+
+
