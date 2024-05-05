599b5d94b57b5d1aadad320d96a3646c7eb3d7a1
yuvraj yadav <yuvraj7000raju@gmail.com>
2024-05-05 11:52:14 +0530

    jwt authentication and auth middleware is done and working fine

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,97 +1,152 @@
 
-import {  User } from "../models/user.model.js"
+import { User } from "../models/user.model.js"
 import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 
-const registerUser = asyncHandler( async (req,res)=>{
-   const{Name, username ,email, passward, intrests} = req.body
-console.log(req.body)
-   if(
-    [Name, username ,email, passward].some((field)=>{
-        return field?.trim() === ""
+const generateRefreshAndAccessToken = async (userID)=>{
+  try {
+    console.log("////", userID)
+    const user = await User.findById(userID)
+
+    console.log("////", user)
+    const accessToken = await user.generateAccessToken()
+    const refreshToken = await user.generateRefreshToken()
+
+    console.log("////", refreshToken,accessToken)
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false })
+
+    return { accessToken, refreshToken }
+  } catch (error) {
+    throw new ApiError(500, "something went wrong while generating access and refresh token")
+  }
+
+}
+
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { Name, username, email, passward, intrests } = req.body
+  console.log(req.body)
+  if (
+    [Name, username, email, passward].some((field) => {
+      return field?.trim() === ""
     })
-   ){
-    throw new ApiError(400,"all fields are required")
-   }
-   
-   const existedUser = await User.findOne({
-    $or : [{username},{email}]
-   })
+  ) {
+    throw new ApiError(400, "all fields are required")
+  }
 
-   if(existedUser){
-    throw new ApiError(409,"user with email or username already existed")
-   }
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }]
+  })
 
-   const imageLocalPath = req.file.path;
-   if(!imageLocalPath){
+  if (existedUser) {
+    throw new ApiError(409, "user with email or username already existed")
+  }
+
+  const imageLocalPath = req.file.path;
+  if (!imageLocalPath) {
     console.log(req.file);
     console.log("nooooooooooooooooooooo")
     console.log(req.file)
-   }
+  }
 
   const image = await uploadOnCloudinary(imageLocalPath)
-  
+
   const user = await User.create({
     Name,
-    username : username.toLowerCase(),
+    username: username.toLowerCase(),
     email,
     passward,
-    intrests : intrests || "add",
-    image : image?.url || ""
+    intrests: intrests || "add",
+    image: image?.url || ""
 
   })
 
   const createdUser = await User.findById(user._id).select(
-    "-passward -refreshToken" 
+    "-passward -refreshToken"
   )
-if(!createdUser){
+  if (!createdUser) {
     throw new ApiError(500, "something went wrong while resistering user")
-}
-return res.status(200).json(
-    new ApiResponse(200,createdUser,"user created successfully")
-)
+  }
+  return res.status(200).json(
+    new ApiResponse(200, createdUser, "user created successfully")
+  )
 
 }
 )
 
-const loginUser = asyncHandler(async(req,res)=>{
-     const {username,email,passward} = req.body
-     console.log(req.body)
+const loginUser = asyncHandler(async (req, res) => {
+  const { username, email, passward } = req.body
+  console.log(req.body)
 
-     if(!email || !username){
-      throw new ApiError(400,"no email or username is given")
-     }
+  if (!email && !username) {
+    throw new ApiError(400, "no email or username is given")
+  }
 
-     const user = await User.findOne({
-      $or :[{username} , {email}]
-     })
+  const user = await User.findOne({
+    $or: [{ username }, { email }]
+  })
 
-     if(!user){
-      throw new ApiError(404,"User not found")
-    }
-    
-    console.log(user)
-     
-     const isPasswardValid = await user.isPasswardCorrect(passward)
+  if (!user) {
+    throw new ApiError(404, "User not found")
+  }
 
-     if( !isPasswardValid ){
-      throw new ApiError(400,"passward is not matched !")
-     }
-     
-     console.log(username)
+  console.log(user)
 
-     
-     console.log(user)
+  const isPasswardValid = await user.isPasswardCorrect(passward)
 
-     res.status(200).json(
-      new ApiResponse(200,user,"User is loged in sussesfully")
-     )
+  if (!isPasswardValid) {
+    throw new ApiError(400, "passward is not matched !")
+  }
+
+  console.log(username)
+  console.log(user)
+console.log("...................", user._id)
+const {refreshToken , accessToken} = await generateRefreshAndAccessToken(user._id);
+
+
+
+const logedinUser = await User.findById(user._id).select("-refreshToken -accessToken");
+const options = {
+  httpOnly : true,
+  secure : true
+}
+
+  res.status(200).
+  cookie("refreshToken",refreshToken,options).
+  cookie("accessToken",accessToken,options).
+  json(
+    new ApiResponse(200, logedinUser, "User is loged in sussesfully")
+  )
 })
 
+const logoutUser = asyncHandler(async (req,res)=>{
+
+  await User.findByIdAndUpdate(req.user._id,{
+    $unset:{
+      refreshToken : 1
+    }},{
+      new : true
+    }
+  )
+
+  const options ={
+    httpOnly : true,
+    secure : true
+  }
+  
+  console.log(req.user)
+
+  res.status(200).clearCookie("accessToken",options).clearCookie("refreshToken",options).json( new ApiResponse(200,{},"user logout succsefully"))
+
+
+})
 
 export {
-    registerUser,
-    loginUser
+  registerUser,
+  loginUser,
+  logoutUser
 }
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -70,7 +70,10 @@ const userSchema = new Schema(
                 trim :true,
                 lowercase :true
             }
-        ]
+        ],
+        refreshToken : {
+            type: String
+        }
     },{
         timestamps: true
     }
@@ -98,7 +101,7 @@ userSchema.methods.generateAccessToken = function(){
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
-            expiresIn : ACCESS_TOKEN_EXPIRY
+            expiresIn : process.env.ACCESS_TOKEN_EXPIRY
         }
     )
 }
@@ -110,9 +113,47 @@ userSchema.methods.generateRefreshToken = function(){
         },
         process.env.REFRESH_TOKEN_SECRET,
         {
-            expiresIn : REFRESH_TOKEN_EXPIRY
+            expiresIn : process.env.REFRESH_TOKEN_EXPIRY
         }
     )
 }
+// userSchema.methods.generateAccessToken = function() {
+//     try {
+//         const accessToken = jwt.sign(
+//             {
+//                 id: this.id,
+//                 email: this.email,
+//                 username: this.username
+//             },
+//             process.env.ACCESS_TOKEN_SECRET,
+//             {
+//                 expiresIn: ACCESS_TOKEN_EXPIRY
+//             }
+//         );
+//         console.log("Access Token generated:", accessToken);
+//         return accessToken;
+//     } catch (error) {
+//         console.error("Error generating Access Token:", error);
+//         throw error; // or handle the error as per your requirement
+//     }
+// };
 
+// userSchema.methods.generateRefreshToken = function() {
+//     try {
+//         const refreshToken = jwt.sign(
+//             {
+//                 id: this.id
+//             },
+//             process.env.REFRESH_TOKEN_SECRET,
+//             {
+//                 expiresIn: REFRESH_TOKEN_EXPIRY
+//             }
+//         );
+//         console.log("Refresh Token generated:", refreshToken);
+//         return refreshToken;
+//     } catch (error) {
+//         console.error("Error generating Refresh Token:", error);
+//         throw error; // or handle the error as per your requirement
+//     }
+// };
 export const User = mongoose.model("user",userSchema)
